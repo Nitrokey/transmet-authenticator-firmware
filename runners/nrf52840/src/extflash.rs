e104ac0717f1b0ace3633157f0ac0bb6a0c170a0
Jan Nordholz <j.nordholz@tu-berlin.de>
2021-09-11 03:40:50 +0200

    Test JEDEC/SFDP/ReadData from external flash.

--- runners/nrf52840/src/extflash.rs
+++ runners/nrf52840/src/extflash.rs
@@ -5,7 +5,10 @@ use nrf52840_hal::{
 	prelude::OutputPin
 };
 
-pub const FLASH_SIZE: usize = 0x2_0000;
+#[cfg(feature = "board-proto1")]
+pub const FLASH_SIZE: usize = 0x20_0000;	/* GD25Q16C, 16 Mbit == 2 MB */
+#[cfg(feature = "board-nrfdk")]
+pub const FLASH_SIZE: usize = 0x80_0000;	/* MX25R6435F, 64 Mbit == 8 MB */
 
 pub struct ExtFlashStorage<SPI> where SPI: Transfer<u8> {
 	// extflash: spi_memory::series25::Flash<SPI, Pin<Output<PushPull>>>,
@@ -22,8 +25,6 @@ impl<SPI> littlefs2::driver::Storage for ExtFlashStorage<SPI> where SPI: Transfe
 	type CACHE_SIZE = generic_array::typenum::U256;
 	type LOOKAHEADWORDS_SIZE = generic_array::typenum::U1;
 
-	// the ReadNorFlash trait exposes a try_read() which (stupidly) expects a mutable self
-	// can't get those two to align - so clone the function and drop the mut there
 	fn read(&self, off: usize, buf: &mut [u8]) -> Result<usize, littlefs2::io::Error> {
 		// rtt_target::rprintln!("F RD {:x} {:x}", off, buf.len());
 		Err(littlefs2::io::Error::Unknown(0x6565_6565))
@@ -39,40 +40,6 @@ impl<SPI> littlefs2::driver::Storage for ExtFlashStorage<SPI> where SPI: Transfe
 		Err(littlefs2::io::Error::Unknown(0x6565_6565))
 	}
 }
-
-/*
-impl<SPI> ExtFlashStorage<SPI> where SPI: Transfer<u8> {
-
-	pub fn new(spim: SPI, cs: Pin<Output<PushPull>>, mut power_pin: Pin<Output<PushPull>>) -> Option<Self> {
-		power_pin.set_high().ok();
-
-		let extflash_r = spi_memory::series25::Flash::init(spim, cs);
-
-		if let Ok(mut extflash) = extflash_r {
-			Some(Self { extflash, power_pin })
-		} else {
-			None
-		}
-	}
-
-	pub fn debug_ident(&mut self) {
-		let status_r = self.extflash.read_status();
-		let jedec_r = self.extflash.read_jedec_id();
-		match status_r {
-		Err(spi_memory::Error::Spi(_)) => { rtt_target::rprintln!("Status: SPI Error"); }
-		Err(spi_memory::Error::Gpio(_)) => { rtt_target::rprintln!("Status: GPIO Error"); }
-		Err(_) => { rtt_target::rprintln!("Status: Other Error"); }
-		Ok(status) => { rtt_target::rprintln!("Status {:?}", status); }
-		}
-		match jedec_r {
-		Err(spi_memory::Error::Spi(_)) => { rtt_target::rprintln!("JEDEC: SPI Error"); }
-		Err(spi_memory::Error::Gpio(_)) => { rtt_target::rprintln!("JEDEC: GPIO Error"); }
-		Err(_) => { rtt_target::rprintln!("JEDEC: Other Error"); }
-		Ok(jedec_id) => { rtt_target::rprintln!("JEDEC {:?}", jedec_id); }
-		}
-	}
-}
-*/
 
 impl<SPI> ExtFlashStorage<SPI> where SPI: Transfer<u8> {
 
--- runners/nrf52840/src/spi_nor_flash.rs
+++ runners/nrf52840/src/spi_nor_flash.rs
@@ -16,14 +16,14 @@ impl<SPI, CS> SpiNorFlash<SPI, CS> where SPI: Transfer<u8>, CS: OutputPin {
 	}
 
 	pub fn init(&mut self, spi: &mut SPI) {
-		let mut buf: [u8; 256] = [0; 256];
+		let mut buf: [u8; 384] = [0; 384];
 
 		loop {
 			buf.fill(0);
 			buf[0] = 0x9f;
 
 			let jedec_r = do_transfer(spi, &mut self.cs, &mut buf[0..12]);
-			if jedec_r.is_ok() {
+			if jedec_r {
 				if buf[1] != 0x00 && buf[1] != 0xff {
 					rtt_target::rprintln!("ExtFlash JEDEC {:02x} {:02x} {:02x}", buf[1], buf[2], buf[3]);
 					break;
@@ -33,14 +33,21 @@ impl<SPI, CS> SpiNorFlash<SPI, CS> where SPI: Transfer<u8>, CS: OutputPin {
 				panic!();
 			}
 		}
-
+/*
 		buf.fill(0);
 		buf[0] = 0x5a;
 		let sfdp_dump = do_transfer(spi, &mut self.cs, &mut buf);
 
-		for i in 0..32 {
-			rtt_target::rprintln!("SFDP HDR {:08x}", u32::from_le_bytes(buf[5+4*i..9+4*i].try_into().unwrap()));
+		for i in 0..(384-5)/8 {
+			rtt_target::rprintln!("SFDP HDR[{:03x}] {:08x} {:08x}", i<<3,
+				u32::from_le_bytes(buf[5+8*i..9+8*i].try_into().unwrap()),
+				u32::from_le_bytes(buf[9+8*i..13+8*i].try_into().unwrap()));
 		}
+*/
+		buf.fill(0);
+		buf[0] = 0x03;
+		let read_data = do_transfer(spi, &mut self.cs, &mut buf);
+		rtt_target::rprintln!("Data: {:x} {:x} {:x}...", buf[0], buf[19], buf[83]);
 	}
 
 	fn read_sfdp(&mut self, spi: &mut SPI, index: u32) -> (u32, u32) {
@@ -60,12 +67,9 @@ impl<SPI, CS> SpiNorFlash<SPI, CS> where SPI: Transfer<u8>, CS: OutputPin {
 }
 
 
-fn do_transfer<SPI, CS>(spi: &mut SPI, _cs: &mut CS, buf: &mut [u8]) -> Result<(), ()> where SPI: Transfer<u8>, CS: OutputPin {
+fn do_transfer<SPI, CS>(spi: &mut SPI, _cs: &mut CS, buf: &mut [u8]) -> bool where SPI: Transfer<u8>, CS: OutputPin {
 	// cs.set_low().ok();
-
-	let r = spi.transfer(buf).map(|_| ()).map_err(|_| ());
-
+	let r = spi.transfer(buf);
 	// cs.set_high().ok();
-
-	r
+	r.is_ok()
 }
